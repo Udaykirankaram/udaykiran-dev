48499b198b2faaba2c658d68882be7c6ebb77cc1
udaykiran <kiranu235@gmail.com>
2026-06-25 15:11:03 +0530

    add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,32 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://udaykiran-dev-lyart.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://udaykiran-dev-lyart.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://udaykiran-dev-lyart.vercel.app/skills",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://udaykiran-dev-lyart.vercel.app/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://udaykiran-dev-lyart.vercel.app/experience",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://udaykiran-dev-lyart.vercel.app/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://udaykiran-dev-lyart.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
