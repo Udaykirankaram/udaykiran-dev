16adc8a124181e782aea070c17e1fb3f585b7df7
udaykiran <kiranu235@gmail.com>
2026-06-30 20:59:23 +0530

    update code

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  const baseUrl = "https://udaykiran-dev-lyart.vercel.app";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/skills`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/experience`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+  ];
+}
